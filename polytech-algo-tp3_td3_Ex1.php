d3713410c8ac633b33f6d694c1149ed280df2867
CheikhJad <cheikhjad@orange.fr>
2024-10-03 11:38:34 +0200

    Update polytech-algo-tp3_td3_Ex1.php
    explication du proces

--- polytech-algo-tp3_td3_Ex1.php
+++ polytech-algo-tp3_td3_Ex1.php
@@ -10,6 +10,6 @@ function factorielWhile($n) {
 }
 
     /* Exemple d'utilisation*/
-$n = (int)readline("Entrez un nombre pour calculer son factoriel : ");
+$n = (int)readline("Entrez un nombre pour calculer son factoriel : "); /* j'au eu l'aide de mon frère pour la recupération de la valeur de la variable */
 echo "Le factoriel de $n est : " . factorielWhile($n) . "\n";
 ?>
